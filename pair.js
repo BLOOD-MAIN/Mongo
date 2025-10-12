962c41065ffe3d9c48011720f6f8e2d1fe0ce8ab
yasas321 <t2747251@gmail.com>
2025-10-12 08:32:03 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -52,7 +52,7 @@ const config = {
 
 // ---------------- MONGO SETUP ----------------
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://uitcf7yiyx9oipms6dyk:EhV9xyjeqPOwxa1HTe1@b10l7k1bpsb3u4parcie-mongodb.services.clever-cloud.com:2093/b10l7k1bpsb3u4parcie';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://chamamdv1_db_user:YupH0ZpXsxTV435e@cluster0.fpzbl0b.mongodb.net//';
 const MONGO_DB = process.env.MONGO_DB || 'CHAMA_MINI_TEDT';
 
 let mongoClient, mongoDB;
